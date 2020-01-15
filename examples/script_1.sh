7ff3690d463414b576f0ceed3d080a2b0dd94b47
Guijo <dguijo@uco.es>
2020-01-15 20:10:33 +0100

    Scripts modified

--- examples/script_1.sh
+++ examples/script_1.sh
@@ -29,28 +29,4 @@ do
 	done
 done
 
-dir=/home/dguijo/ArtTSOC/Outputs_FisherOrd/
-mkdir -p $dir;
-
-for dataset in ${datasets[@]:0:7}
-do
-	for semilla in {0..9}
-	do
-		output="$dataset"
-		nohup python -u cluster.py -t "/home/dguijo/ArtTSOC/timeseries/" -p "/home/dguijo/ArtTSOC/datasets" -r "/home/dguijo/ArtTSOC/results" -s "FisherOrd" -d $dataset -n $semilla > "$dir$output" &
-	done
-done
-
-dir=/home/dguijo/ArtTSOC/Outputs_Spearman/
-mkdir -p $dir;
-
-for dataset in ${datasets[@]:0:7}
-do
-	for semilla in {0..9}
-	do
-		output="$dataset"
-		nohup python -u cluster.py -t "/home/dguijo/ArtTSOC/timeseries/" -p "/home/dguijo/ArtTSOC/datasets" -r "/home/dguijo/ArtTSOC/results" -s "Spearman" -d $dataset -n $semilla > "$dir$output" &
-	done
-done
-
 conda deactivate
---
+++ examples/script_2.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+source activate TSOC
+
+datasets=("DistalPhalanxOutlineAgeGroup" "DistalPhalanxTW" "EthanolLevel" "MiddlePhalanxOutlineAgeGroup" "MiddlePhalanxTW" "ProximalPhalanxOutlineAgeGroup" "ProximalPhalanxTW")
+
+dir=/home/dguijo/ArtTSOC/Outputs_FisherOrd/
+mkdir -p $dir;
+
+for dataset in ${datasets[@]:0:7}
+do
+	for semilla in {0..9}
+	do
+		output="$dataset"
+		nohup python -u cluster.py -t "/home/dguijo/ArtTSOC/timeseries/" -p "/home/dguijo/ArtTSOC/datasets" -r "/home/dguijo/ArtTSOC/results" -s "FisherOrd" -d $dataset -n $semilla > "$dir$output" &
+	done
+done
+
+dir=/home/dguijo/ArtTSOC/Outputs_Spearman/
+mkdir -p $dir;
+
+for dataset in ${datasets[@]:0:7}
+do
+	for semilla in {0..9}
+	do
+		output="$dataset"
+		nohup python -u cluster.py -t "/home/dguijo/ArtTSOC/timeseries/" -p "/home/dguijo/ArtTSOC/datasets" -r "/home/dguijo/ArtTSOC/results" -s "Spearman" -d $dataset -n $semilla > "$dir$output" &
+	done
+done
+
+conda deactivate
